55145c686f6cd8141b672feca22bdd75c6f67019
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2012-07-15 12:46:30 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeocoderTest::Application.config.secret_token = '5ac8c01589a33528ba82e923ca88f1983c8c5a816701ce882fd9b97c154668db4c54fb6676ab6e60549ed03f64d9f5bc60b0c754f96ab4cbc1c9ac77508cf4eb'
